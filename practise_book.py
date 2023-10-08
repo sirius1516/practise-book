63821f0f9c2817e04505add4db603395051fbc44
sirius1516 <gontarenco.s@gmail.com>
2023-10-08 19:37:26 +0200

    hello from decorator homework

--- practise_book.py
+++ practise_book.py
@@ -1,17 +1,16 @@
 from functools import wraps
 
-def my_function(function):
+def hello_from_decorator(function):
     @wraps(function)
     def wrap_function(*args, **kwargs):
-        print(args, ' -This is args')
-        print(kwargs, ' -This is kwargs')
-        return function(*args, **kwargs)
+        firstname = function(*args, **kwargs)
+        return str(firstname) + ' Hello from decorator!'
     return wrap_function
 
 
-@my_function
-def new_function():
-    print('My homework is done!')
+@hello_from_decorator
+def first_name_function():
+    return 'sirius1516'
 
 
-new_function()
+print(first_name_function())
